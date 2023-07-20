d8f4fb08ea9bcb0cb25cac90e1b7efbdf5c3605d
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-07-20 09:45:50 -0300

    .

--- projeto1/src/App.jsx
+++ projeto1/src/App.jsx
@@ -1,13 +1,8 @@
-import { Post } from './Post';
-
-export function app() {
-  return (
-    <div>
-      <post
-      author="Diego Fernandes"
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aperiam dolores quidem ea blanditiis 
-      dolor nihil ipsa quisquam maiores, ad expedita assumenda, cum vitae sit? Delectus iusto ad tempora repudiandae.S" 
-      />
-    </div>
+import Post from './Post'
+function App() {
+  return ( 
+    <Post />
   )
 } 
+
+export default App
--- projeto1/src/main.jsx
+++ projeto1/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Post from './App.jsx'
-import './index.css'
+import { App } from './aa'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
